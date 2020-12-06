e5760247e21f532bc05dbcb6559e03c57c1d39e5
SteffenLm <steffenlm@outlook.com>
2020-12-06 19:57:28 +0100

    added interesting fields

--- corona-info/src/app/coronadata.service.ts
+++ corona-info/src/app/coronadata.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
-import { query } from '@angular/animations';
 import { map } from 'rxjs/operators';
 
 @Injectable({
@@ -21,7 +20,7 @@ export class CoronadataService {
   }
 
   public getLandkreisData(objectId: string) {
-    const query = `query?where=ObjectId=${objectId}&outFields=BEZ,GEN,%20death_rate,%20cases,%20deaths,%20cases_per_100k%20,%20cases_per_population&returnGeometry=false&f=json`;
+    const query = `query?where=ObjectId=${objectId}&outFields=BEZ,GEN,cases,deaths,cases7_per_100k,cases7_bl_per_100k,BL&returnGeometry=false&f=json`;
     return this.httpClient.get(environment.apiUrl + query).pipe(
       map((data: any) => data.features)
     );
