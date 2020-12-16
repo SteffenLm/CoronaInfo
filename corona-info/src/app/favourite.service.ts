dca42595674337b3b34018424acf3081cfb94ac5
SteffenLm <steffenlm@outlook.com>
2020-12-16 19:51:41 +0100

    refactored favourite service

--- corona-info/src/app/favourite.service.ts
+++ corona-info/src/app/favourite.service.ts
@@ -1,41 +1,29 @@
 import { Injectable } from '@angular/core';
+import { LocalStorage } from './local-storage';
 
 @Injectable({
   providedIn: 'root'
 })
-export class FavouriteService {
-
-  static readonly LocalStorageKey = 'favourites';
-
-  private favourites: string[];
-
+export class FavouriteService extends LocalStorage<string> {
+  
   constructor() {
-
-    const favouritesAsString = localStorage.getItem(FavouriteService.LocalStorageKey);
-    if (favouritesAsString === null) {
-      this.favourites = [];
-      this.persistFavourites();
-    } else {
-      this.favourites = JSON.parse(favouritesAsString);
-    }
-  }
-
-  private persistFavourites(): void {
-    localStorage.setItem(FavouriteService.LocalStorageKey, JSON.stringify(this.favourites));
+    super('favourites');
   }
 
   public addFavourite(favourite: string) {
-    this.favourites.push(favourite);
-    this.persistFavourites();
+    const favourites = this.getData();
+    favourites.push(favourite);
+    this.setData(favourites);
   }
 
-  public removeFavourite(objectId: string) {
-    const index = this.favourites.indexOf(objectId);
-    this.favourites.splice(index, 1);
-    this.persistFavourites();
+  public removeFavourite(favourite: string) {
+    const favourites = this.getData();
+    const index = favourites.indexOf(favourite);
+    favourites.splice(index, 1);
+    this.setData(favourites);
   }
 
   public getFavourites(): string[] {
-    return this.favourites.slice(0);
+    return this.getData();
   }
 }
